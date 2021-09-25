f663a33ca7ffa577c56be31b2960eefa039eb6c3
Sevenamber <sevenamber@gmail.com>
2021-09-25 14:07:38 +0300

    fix: path in js 3

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://welbex-test.herokuapp.com/api/delivery';
+const API_URL = new URL('/api/delivery', document.URL);
 
 export default async function loadTable(page, query) {
   const payload = {
